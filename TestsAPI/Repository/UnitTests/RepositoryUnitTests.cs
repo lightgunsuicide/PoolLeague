a41ab6ba4c68da93e34597f320d2881a4e619d9c
slewin <Stephan.Lewin@health.telstra.com>
2018-01-23 16:54:32 +0000

    Start of repository tests and set up

---
+++ TestsAPI/Repository/UnitTests/RepositoryUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using FluentAssertions;
+using LeagueAPI.Configuration;
+using LeagueAPI.Repository;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit; 
+
+namespace TestsAPI.Repository.UnitTests
+{
+     public class RepositoryUnitTests
+    {
+        [Fact]
+        public void FindById()
+        {
+            //Arrange 
+          //  var repository = new PlayerRepository(settings);
+            
+
+            //Act
+
+
+            //Assert
+
+        }
+
+
+    }
+}
