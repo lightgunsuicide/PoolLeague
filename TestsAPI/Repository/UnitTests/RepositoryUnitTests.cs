c748913496db7c60775fd843a18b2bc0635bc6f0
lightgunsuicide <lewin.stephan@gmail.com>
2018-01-25 08:15:19 +0000

    Adding repository tests

--- TestsAPI/Repository/UnitTests/RepositoryUnitTests.cs
+++ TestsAPI/Repository/UnitTests/RepositoryUnitTests.cs
@@ -6,13 +6,26 @@ using FluentAssertions;
 using LeagueAPI.Configuration;
 using LeagueAPI.Repository;
 using Microsoft.Extensions.Options;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using Xunit; 
+using Xunit;
+using LeagueAPI.Application.Dtos.Interfaces;
 
 namespace TestsAPI.Repository.UnitTests
 {
      public class RepositoryUnitTests
     {
+        private readonly IOptions<MongoSettings> config;
+
+        [Fact]
+        public void AddPlayer() {
+            //Arrange
+            var playerRepo = new PlayerRepository(config);
+
+            //Act
+            playerRepo.Add();
+
+            //Assert
+        }
+
         [Fact]
         public void FindById()
         {
