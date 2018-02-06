481570d6752dbe3ff45ed998eed7c645ff6e2290
slewin <Stephan.Lewin@health.telstra.com>
2018-02-06 16:20:35 +0000

    New branch with specflow files for API acceptance tests

--- TestsAcceptance/API/AcceptanceTests/Features/UpdatingPlayerInfo.feature
+++ TestsAcceptance/API/AcceptanceTests/Features/UpdatingPlayerInfo.feature
@@ -5,13 +5,12 @@
 @mytag
 Scenario: Adding a new player
 	Given I make a call to the API requesting to add a new player
-	And I have entered a new username and password
 	When I make the request
 	Then the player is added to the league
 
+
 Scenario: Adding a new game 
-	Given I make a call to the API with data 
+	Given I make a call to the API with new game data 
 	When I add details of a game 
 	Then the winning player increments their win total by one
 	And the losing player increments their losss total by one
-
---
+++ TestsAcceptance/API/AcceptanceTests/Steps/UpdatingPlayerSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace TestsAcceptance.API.AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class UpdatingPlayerSteps
+    {
+        [Given(@"I make a call to the API requesting to add a new player")]
+        public void GivenIMakeACallToTheAPIRequestingToAddANewPlayer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I make the request")]
+        public void WhenIMakeTheRequest()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the player is added to the league")]
+        public void ThenThePlayerIsAddedToTheLeague()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I make a call to the API with new game data")]
+        public void GivenIMakeACallToTheAPIWithNewGameData()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I add details of a game")]
+        public void WhenIAddDetailsOfAGame()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the winning player increments their win total by one")]
+        public void ThenTheWinningPlayerIncrementsTheirWinTotalByOne()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the losing player increments their losss total by one")]
+        public void ThenTheLosingPlayerIncrementsTheirLosssTotalByOne()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
